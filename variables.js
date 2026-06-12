e094182b74fa81b6912ae6b0f8ecf40fe094ca61
Anishapoorani <anishakrishnamoorthi@gmail.com>
2026-06-12 10:26:09 +0530

    variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+// variables using var
+var name = "vetias"
+console.log(name)
+
+//variables using let
+let age = 19
+console.log(age)
+
+//variables using const
+const country = "India"
+console.log(country)
