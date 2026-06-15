b862b3143b1d5fb1c34f869cafff75fbad701fb0
Anishapoorani <anishakrishnamoorthi@gmail.com>
2026-06-15 10:44:05 +0530

    condition.js

---
+++ condition.js
@@ -0,0 +1,35 @@
+var light = "white";
+
+if (light == "Green") {
+    console.log("Go");
+}
+else if (ligth == "Yellow") {
+    console.log("wait");
+}
+else if (light == "Red"){
+    console.log("stop!");
+}
+else {
+    console.log("invalid light");
+}
+console.log("outside if");
+
+//switch statement
+var signal = "White"
+
+switch(signal){
+    case"green":
+    console,log("go");
+    break;
+
+    case"yelloww":
+    console.log("wait");
+    break;
+
+    case "red":
+        console.log("stop");
+        break;
+
+        default:
+            console.log("invalid signal color");
+}
